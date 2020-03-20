25f95c14996b61ee61b418e9a3fb386bf7015ed8
maniakevin <kevincolcha@hotmail.com>
2020-03-20 12:52:37 -0700

    Agregando todos los scripts de la practica 2 junto con la practica y un cambio a la practica 1

---
+++ firebase/basededatoshd.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAiYTvoU1RhovBiclAXhn_eIpGEKf348HY",
+    authDomain: "sistemasgeo67410.firebaseapp.com",
+    databaseURL: "https://sistemasgeo67410.firebaseio.com",
+    projectId: "sistemasgeo67410",
+    storageBucket: "sistemasgeo67410.appspot.com",
+    messagingSenderId: "780202642531",
+    appId: "1:780202642531:web:9d878ca53bb544713bbc56"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
