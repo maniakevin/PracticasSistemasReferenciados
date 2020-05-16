96286546f5ebda9192e28285797e2805bbb7cc83
maniakevin <kevincolcha@hotmail.com>
2020-05-16 15:47:46 -0600

    actualizacion script

--- firebase/Amigos/Scripts/index.js
+++ firebase/Amigos/Scripts/index.js
@@ -12,7 +12,7 @@ const configuraMenu = (user) => {
                <p>Correo: ${ user.email}</p>
                <p>Teléfono: ${ doc.data().telefono }</p>
                <p>Dirección: ${ doc.data().direccion }</p>
-               <p>Coordenadas: ${ doc.data().coordendadas.latitude }, ${doc.data().coordendadas.longitude}</p>
+               <p>Coordenadas: ${ doc.data().coordenadas.latitude }, ${doc.data().coordendadas.longitude}</p>
            `;
            datosdelacuenta.innerHTML = html;
        });
